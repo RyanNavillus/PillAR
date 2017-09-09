0ed77ea3bd100defaebb9e91828812c6d2938d86
Ryan Sullivan <rps214@verizon.net>
2017-09-09 04:39:32 -0400

    Parse google inputs into json

--- PillAR/PillAR/GoogleAPIManager.swift
+++ PillAR/PillAR/GoogleAPIManager.swift
@@ -14,7 +14,7 @@ class GoogleAPIManager {
     let baseURL: NSURL = NSURL(string: "https://vision.googleapis.com")!
     let apiKey = "AIzaSyBNed9n7O5T6hsEJ8lrvZVz-L00Q0F_h3w" // THIS SHOULD NOT BE IN A PUBLIC REPO BUT FUCK IT SHIP IT
     
-    public static var sharedInstance: GoogleAPIManager = {
+    static var sharedInstance: GoogleAPIManager = {
         let apiManager = GoogleAPIManager()
         
         return apiManager
@@ -24,15 +24,46 @@ class GoogleAPIManager {
         return sharedInstance
     }
     
-//    func identifyDrug(image: UIImage) -> (label: NSString, location: CGPoint) {
-//        let requestURL = baseURL.URLByAppendingPathComponent("/v1/images:annotate").URLByAppendingPathComponent("?key=\(apiKey)")
-//        let request = NSMutableURLRequest(URL: requestURL) // URL for accessing vision api
-//        let session = URLSession.sharedSession()
-//
-//        let task = session.dataTaskWithRequest(request) {
-//            data, response, error -> Void in
-//            //let json = NSJSONSerialization.JSONObjectWithData(data, options: <#T##NSJSONReadingOptions#>)
-//        }
-//        return (label: "", location: CGPoint())
-//    }
+    func identifyDrug(image: UIImage, completionHandler: (NSString, CGPoint) -> ()) {
+        guard let apiURL = baseURL.appendingPathComponent("/v1/images:annotate") else {
+            print("Failed to create url for identifyDrug")
+            return
+        }
+        let requestURL = apiURL.appendingPathComponent("?key=\(apiKey)")
+        let request = NSMutableURLRequest(url: requestURL) // URL for accessing vision api
+        let session = URLSession.shared
+        
+        let imageData = UIImagePNGRepresentation(image)?.base64EncodedString(options: .lineLength64Characters)
+        
+        let json: [String: Any] = [
+            "requests" : [
+                "image" : [
+                    "content" : "\(imageData)"
+                ],
+                "features" : [
+                    [
+                        "type" : "LOGO_DETECTION",
+                        "maxResults" : 3
+                    ]
+                ]
+            ]
+        ]
+        if JSONSerialization.isValidJSONObject(json) {
+            do {
+                let jsonData = try JSONSerialization.data(withJSONObject: json, options: .prettyPrinted)
+                request.httpBody = jsonData
+            } catch {
+                
+            }
+        } else {
+            print("Invalid json created for identifyDrug")
+            return
+        }
+        let task = session.dataTask(with: request as URLRequest) {
+            data, response, error -> Void in
+            //Parse Json for response
+            //let json = NSJSONSerialization.JSONObjectWithData(data, options: <#T##NSJSONReadingOptions#>)
+        }
+        return
+    }
 }

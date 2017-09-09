cf95e3cd60c02885899c6dd1b318abffa3e0e0b7
Yasmeen Roumie <y4smeen@gmail.com>
2017-09-09 18:56:37 -0400

    Get frequency and max dosage information using regex functions

--- python-flask/app/src/appScraper.py
+++ python-flask/app/src/appScraper.py
@@ -4,7 +4,7 @@ import urllib2, json
 from bs4 import BeautifulSoup
 import re
 
-dosageHeadings = ["Adult:", "Children:", "Adults and Children:"]
+dosageHeadings = ["Adult:", "Adults and Children:"]
 
 @app.route("/")
 @app.route("/medication/<medication>")
@@ -51,8 +51,9 @@ def getMedicationInfo(medication):
                 h2 = h2.strip(':')
                 #print (h2 + " " + p.get_text())
                 #medicationInfo.append(h2 + " " + p.get_text())
-                medicationInfo[h2.strip(':')] = pText
-
+                #medicationInfo[h2.strip(':')] = pText
+                medicationInfo['frequency'] = str(findFreq(pText))
+                medicationInfo['maximum'] = str(findMax(pText))
                 # if 'once daily' in pText:
                 #     medicationInfo[h2.strip(':') + " dosage"] += "once daily"
                 # elif 'every ' and ' hours' in pText:
@@ -68,18 +69,42 @@ def getMedicationInfo(medication):
             pIndex += 1
 
     #return newHTML
+    #frequency, max, num of mg per pill
     return json.dumps(medicationInfo)
 
 # Returns an integer number of pills needed daily given json input
 def findFreq(text):
     if 'once daily' in text or 'once a day' in text or 'one a day' in text or 'one time a day' in text or '1 a day' in text:
-        return 1
+        return "once a day"
     if 'twice daily' in text or 'twice a day' in text or 'two a day' in text or 'two times a day' in text or '2 a day' in text:
-        return 2
+        return "twice a day"
     if re.search(r'every \d hours for \d doses', text, re.I|re.M) is not None:
         return int(re.search(r'every \d hours for \d doses', text, re.I|re.M).group()[-7])
     if re.search(r'every \d hours for \d\d doses', text, re.I|re.M) is not None:
         return int(re.search(r'every \d hours for \d\d doses', text, re.I|re.M).group()[-8:-6])
+    # if re.search(r'max \d\d', text, re.I|re.M) is not None:
+    #     num = int(re.search(r'max \d\d', text, re.I|re.M).group()[4:6])
+    #     return round(num/0.5)
+    # if re.search(r'max \d dose', text, re.I|re.M) is not None or re.search(r'max \d tab', text, re.I|re.M) is not None:
+    #     return int(re.search(r'max \d', text, re.I|re.M).group()[4])
+    # if re.search(r'max \d', text, re.I|re.M) is not None:
+    #     num = int(re.search(r'max \d', text, re.I|re.M).group()[4])
+    #     return round(num/0.3)
+    # if re.search(r'max: \d', text, re.I|re.M) is not None:
+    #     return int(re.search(r'max: \d', text, re.I|re.M).group()[5])
+    if re.search(r'every \d hours', text, re.I|re.M) is not None:
+        num = int(re.search(r'every \d hours', text, re.I|re.M).group()[6])
+        return round(24/num)
+    if re.search(r'every \d\d hours', text, re.I|re.M) is not None:
+        num = int(re.search(r'every \d\d hours', text, re.I|re.M).group()[6:8])
+        return round(24/num)
+    return 1
+
+def findMax(text):
+    if 'once daily' in text or 'once a day' in text or 'one a day' in text or 'one time a day' in text or '1 a day' in text:
+        return 1
+    if 'twice daily' in text or 'twice a day' in text or 'two a day' in text or 'two times a day' in text or '2 a day' in text:
+        return 2
     if re.search(r'max \d\d', text, re.I|re.M) is not None:
         num = int(re.search(r'max \d\d', text, re.I|re.M).group()[4:6])
         return round(num/0.5)
@@ -90,10 +115,4 @@ def findFreq(text):
         return round(num/0.3)
     if re.search(r'max: \d', text, re.I|re.M) is not None:
         return int(re.search(r'max: \d', text, re.I|re.M).group()[5])
-    if re.search(r'every \d hours', text, re.I|re.M) is not None:
-        num = int(re.search(r'every \d hours', text, re.I|re.M).group()[6])
-        return round(24/num)
-    if re.search(r'every \d\d hours', text, re.I|re.M) is not None:
-        num = int(re.search(r'every \d\d hours', text, re.I|re.M).group()[6:8])
-        return round(24/num)
     return 1

5e26b5bbd800f66b2bffbdc9683bd1a52506d561
Yasmeen Roumie <y4smeen@gmail.com>
2017-09-09 20:05:40 -0400

    Return max dosage and instructions

--- python-flask/app/src/appScraper.py
+++ python-flask/app/src/appScraper.py
@@ -52,8 +52,8 @@ def getMedicationInfo(medication):
                 #print (h2 + " " + p.get_text())
                 #medicationInfo.append(h2 + " " + p.get_text())
                 #medicationInfo[h2.strip(':')] = pText
-                medicationInfo['frequency'] = str(findFreq(pText))
-                medicationInfo['maximum'] = str(findMax(pText))
+                medicationInfo['instructions'] = pText
+                medicationInfo['maximum'] = findMax(pText)
                 # if 'once daily' in pText:
                 #     medicationInfo[h2.strip(':') + " dosage"] += "once daily"
                 # elif 'every ' and ' hours' in pText:
@@ -73,46 +73,31 @@ def getMedicationInfo(medication):
     return json.dumps(medicationInfo)
 
 # Returns an integer number of pills needed daily given json input
-def findFreq(text):
+def findMax(text):
     if 'once daily' in text or 'once a day' in text or 'one a day' in text or 'one time a day' in text or '1 a day' in text:
-        return "once a day"
+        #return "once a day"
+        return 1
     if 'twice daily' in text or 'twice a day' in text or 'two a day' in text or 'two times a day' in text or '2 a day' in text:
-        return "twice a day"
+        # return "twice a day"
+        return 2
     if re.search(r'every \d hours for \d doses', text, re.I|re.M) is not None:
         return int(re.search(r'every \d hours for \d doses', text, re.I|re.M).group()[-7])
     if re.search(r'every \d hours for \d\d doses', text, re.I|re.M) is not None:
         return int(re.search(r'every \d hours for \d\d doses', text, re.I|re.M).group()[-8:-6])
-    # if re.search(r'max \d\d', text, re.I|re.M) is not None:
-    #     num = int(re.search(r'max \d\d', text, re.I|re.M).group()[4:6])
-    #     return round(num/0.5)
-    # if re.search(r'max \d dose', text, re.I|re.M) is not None or re.search(r'max \d tab', text, re.I|re.M) is not None:
-    #     return int(re.search(r'max \d', text, re.I|re.M).group()[4])
-    # if re.search(r'max \d', text, re.I|re.M) is not None:
-    #     num = int(re.search(r'max \d', text, re.I|re.M).group()[4])
-    #     return round(num/0.3)
-    # if re.search(r'max: \d', text, re.I|re.M) is not None:
-    #     return int(re.search(r'max: \d', text, re.I|re.M).group()[5])
-    if re.search(r'every \d hours', text, re.I|re.M) is not None:
-        num = int(re.search(r'every \d hours', text, re.I|re.M).group()[6])
-        return round(24/num)
-    if re.search(r'every \d\d hours', text, re.I|re.M) is not None:
-        num = int(re.search(r'every \d\d hours', text, re.I|re.M).group()[6:8])
-        return round(24/num)
-    return 1
-
-def findMax(text):
-    if 'once daily' in text or 'once a day' in text or 'one a day' in text or 'one time a day' in text or '1 a day' in text:
-        return 1
-    if 'twice daily' in text or 'twice a day' in text or 'two a day' in text or 'two times a day' in text or '2 a day' in text:
-        return 2
     if re.search(r'max \d\d', text, re.I|re.M) is not None:
         num = int(re.search(r'max \d\d', text, re.I|re.M).group()[4:6])
-        return round(num/0.5)
+        return int(round(num/0.5))
     if re.search(r'max \d dose', text, re.I|re.M) is not None or re.search(r'max \d tab', text, re.I|re.M) is not None:
         return int(re.search(r'max \d', text, re.I|re.M).group()[4])
     if re.search(r'max \d', text, re.I|re.M) is not None:
         num = int(re.search(r'max \d', text, re.I|re.M).group()[4])
-        return round(num/0.3)
+        return int(round(num/0.3))
     if re.search(r'max: \d', text, re.I|re.M) is not None:
         return int(re.search(r'max: \d', text, re.I|re.M).group()[5])
+    if re.search(r'every \d hours', text, re.I|re.M) is not None:
+        num = int(re.search(r'every \d hours', text, re.I|re.M).group()[6])
+        return int(round(24/num))
+    if re.search(r'every \d\d hours', text, re.I|re.M) is not None:
+        num = int(re.search(r'every \d\d hours', text, re.I|re.M).group()[6:8])
+        return int(round(24/num))
     return 1
